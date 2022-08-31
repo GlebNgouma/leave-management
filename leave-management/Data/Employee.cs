7c106615cb927230ea14fa6648a1ce31c7df54ec
Gleb Ngouma <glebngouma@gmail.com>
2022-08-31 08:24:17 +0100

    Ajoutez des fichiers projet.

---
+++ leave-management/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+namespace leave_management.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string Firstame { get; set; }
+        public string Lastname { get; set; }
+        public string TaxId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
